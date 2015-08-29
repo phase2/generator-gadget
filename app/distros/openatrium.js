a66982112880617a691aa823aeaf5a5189433b08
Grayside <aross@phase2technology.com>
2015-08-29 00:42:59 -0700

    Correct Atrium makefile assembly.

--- app/distros/openatrium.js
+++ app/distros/openatrium.js
@@ -22,8 +22,10 @@ function init() {
   };
 
   module.drushMakeFile = function(yo, done) {
+    var releaseVersion = yo.drupalDistroRelease.match(/^\d+\.x\-(.+)/)[1];
+
     var tokens = {
-      drupalDistroRelease: yo.drupalDistroRelease,
+      drupalDistroRelease: releaseVersion,
       coreCompatibility: yo.drupalDistroVersion
     };
     yo.fs.copyTpl(
@@ -33,12 +35,13 @@ function init() {
     );
 
     // The Core Makefile is managed by Atrium and varies by release.
+    var filename = 'drupal-org-core.make?id=' + yo.drupalDistroRelease;
     yo.fetch(
-      'http://cgit.drupalcode.org/openatrium/plain/drupal-org-core.make?id=' + yo.drupalDistroRelease,
+      'http://cgit.drupalcode.org/openatrium/plain/' + filename,
       'src',
       function () {
-        yo.fs.copy('src/drupal-org-core.make?id=' + yo.drupalDistroRelease, 'src/drupal-org-core.make');
-        yo.fs.delete('src/drupal-org-core.make?id=' + yo.drupalDistroRelease);
+        yo.fs._copySingle('src/' + filename, 'src/drupal-org-core.make');
+        yo.fs.delete('src/' + filename);
         done();
       }
     );
