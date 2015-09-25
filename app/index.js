1b96cac5f0eec87600eae4b70592cc256447895c
Grayside <aross@phase2technology.com>
2015-09-25 14:12:30 -0700

    Added missing pieces that were breaking the generator.

--- app/index.js
+++ app/index.js
@@ -60,7 +60,7 @@ module.exports = yeoman.generators.Base.extend({
   // Install Grunt Drupal Tasks, either the latest published version or the
   // current development version in the master branch.
   installGDT: function() {
-    //require('./gdt')(this).install();
+    require('./gdt')(this).install();
   },
 
   // Determine the latest stable release for the requested Drupal core version.
