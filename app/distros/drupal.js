9c3989a54264ee547b3e807b43f3c7526e159f71
Evan Lovely <evan@evanlovely.com>
2015-10-01 12:45:32 -0700

    semicolon fixes

--- app/distros/drupal.js
+++ app/distros/drupal.js
@@ -2,7 +2,7 @@ function init() {
   var module = {
     id: 'drupal',
     profile: 'standard'
-  }
+  };
 
   module.option = {
     name: 'Drupal',
@@ -18,7 +18,7 @@ function init() {
 
   module.whenCallback = function(answers) {
     return answers.drupalDistro == 'drupal';
-  }
+  };
 
   module.releaseVersion = function(majorVersion, done, cb) {
     require('../drupalProjectVersion').latestRelease(module.id, majorVersion, done, cb);
@@ -36,9 +36,9 @@ function init() {
     );
 
     done();
-  }
+  };
 
   return module;
-};
+}
 
 module.exports = init();
