31a1b058003fef15c2d3208f16e2eb893c01aff8
Grayside <aross@phase2technology.com>
2015-11-23 09:56:06 -0800

    Fix local tests and expand file assert coverage.

--- test/generator.int.test.js
+++ test/generator.int.test.js
@@ -9,17 +9,25 @@ describe('gadget:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({'skip-install': true})
-      .withPrompt({
+      .withOptions({
+        'skip-install': true,
+        projectName: 'drupal8',
+        projectDescription: 'test drupal8 project',
         drupalDistro: 'drupal',
-        'drupalDistroVersion-drupal': '8.0.x'
+        drupalDistroVersion: '8.x'
       })
       .on('end', done);
   });
 
   it('creates files for Drupal 8.x', function () {
     assert.file([
+      // Distribution-specific makefile.
       'src/project.make',
+      // gtd scaffolding dotfiles are copying.
+      'src/modules/.gitkeep',
+      // General-purpose behat.yml is not overridden.
+      'test/behat.yml',
+      // Behat example tests are present.
       'test/features/example.feature'
     ]);
   });
