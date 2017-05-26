263243f123585000290ff70f626eb17a61d523c2
Adam Ross <aross@phase2technology.com>
2017-05-25 17:10:36 -0700

    Add a default description, which effectively makes the description required.

--- generators/lib/prompts.js
+++ generators/lib/prompts.js
@@ -7,6 +7,7 @@ var chalk = require('chalk');
 var _ = require('lodash');
 
 var distros = require('./distros');
+var labelMaker = require('./util').labelMaker;
 
 var prompts = [
   {
@@ -16,13 +17,21 @@ var prompts = [
     // Name of the parent directory.
     default: _.last(process.cwd().split('/')),
     validate: function (input) {
-      return (input.search(' ') === -1) ? true : 'No spaces allowed.';
+      if (input.search(' ') !== -1) return 'No spaces allowed.';
+      return true;
     }
   },
   {
     type: 'input',
     name: 'projectDescription',
-    message: 'One-line project description?'
+    message: 'One-line project description?',
+    validate: function(input) {
+      if (input.search('"') !== -1) return 'No quotes allowed.';
+      return true;
+    },
+    default: function(answers) {
+      return labelMaker(answers['projectName']) + ' Drupal codebase.';
+    }
   },
 
   {
